50c3a7744276a927a07a8b005eaf9ea503ed5415
Rodrigo Amaral <amaralrodrigo798@gmail.com>
2024-06-21 19:05:45 +0100

    generateBoard() function made

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 const main = document.querySelector("main");
+const startGameBtn = document.querySelector(".start-game");
+
+function generateBoard() {
+    const boardContainer = document.querySelector('.board-container');
+
+    if(boardContainer.children.length > 0) return;
+
+    for (let i = 0; i < 9; i++) {
+        const cell = document.createElement('button');
+        cell.classList.add('board-cell');
+
+        boardContainer.appendChild(cell);
+    }
+}
+
+startGameBtn.addEventListener('click', generateBoard);
