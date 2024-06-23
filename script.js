165c1a997389d572e1b52266959f20d9dbfd0547
Rodrigo Amaral <amaralrodrigo798@gmail.com>
2024-06-23 19:41:29 +0100

    Script logic almost done

--- script.js
+++ script.js
@@ -1,19 +1,53 @@
 const main = document.querySelector("main");
 const startGameBtn = document.querySelector(".start-game");
+const gameBoard = document.querySelector('.board-container');
+let currentPlayer = "X";
+let board = ['', '', '', '', '', '', '', '', ''];
 
 function generateBoard() {
-    const boardContainer = document.querySelector('.board-container');
 
-    if(boardContainer.children.length > 0) return;
+    if(gameBoard.children.length > 0) return;
 
     for (let i = 0; i < 9; i++) {
         const cell = document.createElement('button');
-        cell.classList.add('board-cell');
-
+        cell.classList.add('cell');
         cell.setAttribute('data-index', i);
-
-        boardContainer.appendChild(cell);
+        gameBoard.appendChild(cell);
     }
+}
+
+gameBoard.addEventListener('click', (event) => {
+    const target = event.target;
+    const index = target.getAttribute('data-index');
+
+    if(target.classList.contains('cell') && board[index] === '') {
+        board[index] = currentPlayer;
+        target.textContent = currentPlayer;
+        if(checkWin()) {
+            // #TODO: change the alert to a stats board
+            alert(currentPlayer + ' wins!');
+        } else if (board.every(cell => cell !== '')) {
+            alert("It's a draw!");
+        }
+        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+    }
+});
+
+function checkWin() {
+    const winPatterns = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    return winPatterns.some(pattern => {
+        return pattern.every(index => board[index] === currentPlayer);
+    });
 }
 
 startGameBtn.addEventListener('click', generateBoard);
